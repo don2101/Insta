a6d54adf2771e6e43cc088ee8a4984b672def72f
Hee Don Yeom <don210198@gmail.com>
2019-04-15 02:06:28 +0000

    로그인 기능 추가: 유저의 정보를 받아 로그인 시키는 기능 구현 & templates에 유저 정보를 받는 form 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import authenticate
 
 
 # Create your views here.
 def login(request):
-    pass
+    if request.method == "POST":
+        #로그인 검증
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        login_user = authenticate(request, username=username, password=password)
+        #로그인 시킨다
+        if login_user is not None:
+            auth_login(request, login_user)
+            return redirect('posts:list')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    pass
+    if request.user:
+        auth_logout(request)
+    
+    return redirect('posts:list')
+        
