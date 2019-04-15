624cf1113935ab666b28e09dbb0b3d9cca5c12b6
Hee Don Yeom <don210198@gmail.com>
2019-04-15 01:40:55 +0000

    app 생성: 로그인용 어플리케이션인 accounts 생성

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+
+
+# Create your views here.
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
