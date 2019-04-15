9bd1269f54f8f46b69db2609577d0d9685a5edde
Hee Don Yeom <don210198@gmail.com>
2019-04-15 04:42:09 +0000

    기능 추가: 회원가입 구현 & template 구성 & navbar에 추가

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -37,4 +38,18 @@ def logout(request):
     auth_logout(request)
     
     return redirect('posts:list')
+    
+    
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+
+            auth_login(request, user)
+            return redirect('posts:list')
+    else:
+        form = UserCreationForm()
+        return render(request, 'accounts/signup.html', {'form': form})
         
