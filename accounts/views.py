ef2e03ee5f1f92f1a27ec0926d2ca5dbb550f0d7
Hee Don Yeom <don210198@gmail.com>
2019-04-15 02:23:11 +0000

    login template 수정 & django 설정 사용한 login 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -2,27 +2,35 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 
 # Create your views here.
 def login(request):
     if request.method == "POST":
         #로그인 검증
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
         
-        login_user = authenticate(request, username=username, password=password)
-        #로그인 시킨다
-        if login_user is not None:
-            auth_login(request, login_user)
+        # login_user = authenticate(request, username=username, password=password)
+        
+        # 더 쉬운 방법
+        form = AuthenticationForm(request, request.POST)
+        
+        if form.is_valid():
+            auth_login(request, form.get_user())
             return redirect('posts:list')
+        #로그인 시킨다
+        # if login_user is not None:
+        #     auth_login(request, login_user)
+        #     return redirect('posts:list')
     else:
-        return render(request, 'accounts/login.html')
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form': form})
 
 
 def logout(request):
-    if request.user:
-        auth_logout(request)
+    auth_logout(request)
     
     return redirect('posts:list')
         
