52ff77ba0a78f5dffe0962e821aa89fd144a9c0f
Hee Don Yeom <don210198@gmail.com>
2019-04-16 04:10:34 +0000

    instagram/urls.py 오타 수정: people주소 / 추가

--- instagram/urls.py
+++ instagram/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
-    path('<str:username>', accounts_view.people, name="people"),
+    path('<str:username>/', accounts_view.people, name="people"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
